5216d042512bab0e7fadd60777df919f328ebf6a
duncan-morton <duncan@zear.co.uk>
2026-02-20 16:51:06 +0000

    fix vercel tracing for sparticuz chromium assets

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/screenshot": [
+      "./node_modules/@sparticuz/chromium/**/*",
+      "./node_modules/puppeteer-core/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
